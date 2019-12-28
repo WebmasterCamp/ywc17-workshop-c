335788adb4b29601986f8817a952b3d3bc0dd07d
unknown <ktpunnisa@gmail.com>
2019-12-28 15:38:37 +0700

    c

--- src/shared/data/user.js
+++ src/shared/data/user.js
@@ -2,44 +2,14 @@ export default {
     name: 'Somchai Rackchart',
     subjects: [
         {
-            code: 'ค22101',
-            name: 'คณิตศาสตร์พื้นฐาน 2',
+            code: 'ส22101',
+            name: 'สังคมศึกษา ศาสนา และวัฒนธรรม 2',
             colors: ['#32529D', '#C5AACD'],
             courses: [
                 {
                     id: '1',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
-                    questionLength: 20
-                },
-                {
-                    id: '2',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
-                    questionLength: 20
-                },
-                {
-                    id: '3',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
-                    questionLength: 20
-                },
-                {
-                    id: '1',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
-                    questionLength: 20
-                },
-                {
-                    id: '1',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
-                    questionLength: 20
-                },
-                {
-                    id: '1',
-                    name: 'สมการเชิงเส้นพื้นฐาน',
-                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    name: 'เศรษฐศาสตร์',
+                    description: 'แบบทดสอบหลังเรียน',
                     questionLength: 20
                 }
             ]
