2046acf4b199371804d83480d8f706cc7985dc95
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 11:16:53 +0700

    c

---
+++ src/shared/data/user.js
@@ -0,0 +1,46 @@
+export default {
+    name: 'Somchai Rackchart',
+    subjects: [
+        {
+            name: 'ค22101 คณิตศาสตร์พื้นฐาน 2',
+            courses: [
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                },
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                },
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                },
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                },
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                },
+                {
+                    id: '1',
+                    name: 'สมการเชิงเส้นพื้นฐาน',
+                    description: 'แบบทดสอบก่อนเรียน พื้นฐานบทเรียน',
+                    questionLength: 20
+                }
+            ]
+        }
+    ]
+}
