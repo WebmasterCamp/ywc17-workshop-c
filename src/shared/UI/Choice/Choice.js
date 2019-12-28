d6d47614031d39d69561f5b9b31c92f295c27178
unknown <ktpunnisa@gmail.com>
2019-12-28 17:33:15 +0700

    c

--- src/shared/UI/Choice/Choice.js
+++ src/shared/UI/Choice/Choice.js
@@ -19,9 +19,6 @@ export default function Choice(props) {
       className={`${styles.container} ${selected}`}
       onClick={chooseAnswerHandler}
     >
-{/* <div>isAnswer:{props.isAnswer ? 'true' : 'false'}</div>
-<div>isCorrect:{props.isCorrect  ? 'true' : 'false'}</div>
-<div>isSelect:{props.isselect  ? 'true' : 'false'}</div> */}
       {props.isAnswer && props.isCorrect && (
         <div className={styles.colorBarGreen}>
           <div className={styles.circleWhite}></div>
@@ -32,12 +29,7 @@ export default function Choice(props) {
           <div className={styles.circleWhite}></div>
         </div>
       )}
-      {!props.isAnswer && props.isSelect && (
-        <div className={styles.colorBarSelected}>
-          <div className={styles.circleWhite}></div>
-        </div>
-      )}
-      {!props.isAnswer && !props.isSelect && (
+      {!props.isAnswer && (
         <div className={styles.colorBar}>
           <div className={styles.circle}></div>
         </div>
