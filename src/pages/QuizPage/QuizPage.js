67b3437e12f94a60d5a2eb34612e0c6fd93c87d5
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 11:03:19 +0700

    c

--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
-import styles from './QuizPage.module.css';
-
-import QuestionNavigation from '../../shared/UI/QuestionNavigation/QuestionNavigation';
+// import styles from './QuizPage.module.css';
 
 import MOCK_QUESTIONS from '../../shared/data/questions';
+
 import Question from '../../shared/UI/Question/Question';
+import QuestionNavigation from '../../shared/UI/QuestionNavigation/QuestionNavigation';
 
 export default function QuizPage() {
     const [questions, setQuestions] = useState(MOCK_QUESTIONS);
