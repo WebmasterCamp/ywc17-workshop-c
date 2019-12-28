d73e029258d71e3fec44342bdcf4a3f1e3f23cb0
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 17:21:04 +0700

    c

--- src/shared/UI/Logo/Logo.js
+++ src/shared/UI/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Logo.module.css'
+import logo from './logo.png'
 
 export default function Logo() {
     return (
-        <div style={{width: '50px', height: '50px', background: 'grey'}}>
-            Logo
+        <div style={{width: '150px', height: '50px', background: 'grey', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <img src={logo} style={{width: '100%'}} />
         </div>
     )
 }
