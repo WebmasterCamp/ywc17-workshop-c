176a2ae3d617a2cedcc97ba99777bcd6dcee6c89
unknown <ktpunnisa@gmail.com>
2019-12-28 12:02:12 +0700

    c

--- src/shared/UI/Card/Card.js
+++ src/shared/UI/Card/Card.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from "react";
+import styles from "./Card.module.css";
 
-export default function Card() {
-    return (
-        <div>
-            <h1></h1>
-            <h2></h2>
-            <p></p>
-            <button></button>
+import Button from "../Button/Button";
+
+export default function Card(props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.colorBar} style={{background: `transparent linear-gradient(180deg, ${props.color[0]} 0%, ${props.color[1]} 100%) 0% 0% no-repeat padding-box`}} />
+      <div className={styles.course}>
+        <h1>{props.course.name}</h1>
+        <h2>{props.course.description}</h2>
+        <div className={styles.bottom}>
+        <p>{props.course.questionLength}</p>
+        <Button data="เริ่มแบบทดสอบ"></Button>
         </div>
-    )
+
+      </div>
+    </div>
+  );
 }
