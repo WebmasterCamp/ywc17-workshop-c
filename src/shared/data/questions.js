e04a575173f66063aaa2522efba364cd7edef18b
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 09:49:46 +0700

    c

---
+++ src/shared/data/questions.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        type: 'mc',
+        question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut magna nibh. Curabitur neque tortor, dignissim elementum turpis id, vulputate vulputate magna.',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 0,
+        explanation: ['A Lorem ipsum dolor sit amet', 'B Lorem ipsum dolor sit amet', 'C Lorem ipsum dolor sit amet', 'D Lorem ipsum dolor sit amet'],
+        topics: ['th-ch1-1', 'th-ch2-7'],
+        bookmark: false
+    }
+]
