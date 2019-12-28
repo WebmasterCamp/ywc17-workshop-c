52628359d8ba4240fcfb6aec24e0b6b9163ff1b4
unknown <ktpunnisa@gmail.com>
2019-12-28 10:36:32 +0700

    c

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -3,17 +3,12 @@ import React from 'react'
 import styles from './DashboardPage.module.css';
 
 import Button from '../../shared/UI/Button/Button';
-import Question from '../../shared/UI/Question/Question';
-
-import questions from '../../shared/data/questions';
 
 
 export default function DashboardPage() {
     return (
         <div>
             DashboardPage
-            <Button />
-            {questions && questions.map(question => <Question question={question} isAnswer={false} />)}
         </div>
     )
 
--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 import styles from './QuizPage.module.css';
 
+import Question from '../../shared/UI/Question/Question';
+import questions from '../../shared/data/questions';
+
 export default function QuizPage() {
     return (
         <div>
-            QuizPage
+            {questions && questions.map(question => <Question question={question} isAnswer={false} />)}
         </div>
     )
 }
