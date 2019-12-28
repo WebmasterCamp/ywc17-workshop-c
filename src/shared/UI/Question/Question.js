bb016b9cac88a628d3b592995e406ab40c79e4e9
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 13:39:30 +0700

    c

--- src/shared/UI/Question/Question.js
+++ src/shared/UI/Question/Question.js
@@ -6,12 +6,16 @@ import Choice from '../Choice/Choice';
 import AnswerExplanation from '../AnswerExplanation/AnswerExplanation';
 
 export default function Question(props) {
+    const answerQuestionHandler = (choiceIndex) => {
+        props.onAnswer(props.questionNumber, choiceIndex);
+    }
+
     return (
         <div className={styles.container}>
             <h2 className={styles.question}>{props.questionNumber} {props.question.question}</h2>
             <div className={styles.choices}>
                 {
-                    props.question.choices && props.question.choices.map((choice, index) => <Choice key={index} data={choice}></Choice>)
+                    props.question.choices && props.question.choices.map((choice, index) => <Choice key={index} data={choice} index={index} onChoose={answerQuestionHandler} />)
                 }
             </div>
             {!props.isAnswer && <div className={styles.navigation}>
