a5ea3ec5415d0198cd1b6847cee40ee23d6e1efe
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 13:12:32 +0700

    c

--- src/shared/UI/QuestionNavigation/QuestionNavigation.js
+++ src/shared/UI/QuestionNavigation/QuestionNavigation.js
@@ -5,7 +5,7 @@ import QuestionStatus from './QuestionStatus/QuestionStatus';
 export default function QuestionNavigation(props) {
     let QuestionStatusList = [];
     for (let i=1; i <= props.numberofQuestion; i++) {
-        QuestionStatusList.push(<QuestionStatus key={i} questionNumber={i} status="done" isMarked={true} onClick={props.onChangeQuestion.bind(null, i + 1)} disabled={parseInt(currentQuestionNumber) === i} />)
+        QuestionStatusList.push(<QuestionStatus key={i} questionNumber={i} status="done" isMarked={true} onClick={props.onChangeQuestion.bind(null, i + 1)} disabled={parseInt(props.currentQuestionNumber) === i} />)
     }
 
     return (
