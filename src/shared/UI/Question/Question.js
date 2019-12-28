17c83701d28b324da662a361de5784e402dcb437
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 15:35:03 +0700

    c

--- src/shared/UI/Question/Question.js
+++ src/shared/UI/Question/Question.js
@@ -1,44 +1,69 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-regular-svg-icons';
-import { faBookmark as solidBookmark } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+import { faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
 
-import styles from './Question.module.css';
+import styles from "./Question.module.css";
 
-import Button from '../Button/Button';
-import Choice from '../Choice/Choice';
-import AnswerExplanation from '../AnswerExplanation/AnswerExplanation';
+import Button from "../Button/Button";
+import Choice from "../Choice/Choice";
+import AnswerExplanation from "../AnswerExplanation/AnswerExplanation";
 
 export default function Question(props) {
-    const answerQuestionHandler = (choiceIndex) => {
-        props.onAnswer(props.questionNumber, choiceIndex);
+  const answerQuestionHandler = choiceIndex => {
+    props.onAnswer(props.questionNumber, choiceIndex);
+  };
+
+  let history = useHistory();
+
+  const submitAnswerHandler = () => {
+    if (window.confirm("คุณแน่ใจหรือไม่ที่จะส่งคำตอบ")) {
+      history.push("/summary");
     }
+  };
 
-    let history = useHistory();
-
-    const submitAnswerHandler = () => {
-       if (window.confirm("คุณแน่ใจหรือไม่ที่จะส่งคำตอบ")) {
-        history.push("/summary");
-       }
-    }
-
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.question}>{props.questionNumber}. {props.question.question}</h2>
-            {props.isBookmark && (props.isBookmark ? <FontAwesomeIcon icon={solidBookmark} onClick={props.onBookmark.bind(null, props.questionNumber)} /> : <FontAwesomeIcon icon={faBookmark} onClick={props.onBookmark.bind(null, props.questionNumber)} />)}
-            <div className={styles.choices}>
-                {
-                    props.question.choices && props.question.choices.map((choice, index) => <Choice key={index} data={choice} index={index} onChoose={answerQuestionHandler} />)
-                }
-            </div>
-            {!props.isAnswer && <div className={styles.navigation}>
-                {!props.isFirst && <Button onClick={props.onPrevQuestion}>ข้อก่อนหน้า</Button>}
-                {!props.isLast && <Button onClick={props.onNextQuestion}>ข้อถัดไป</Button>}
-                {props.isLast && <Button onClick={submitAnswerHandler}>ส่งคำตอบ</Button>}
-            </div>}
-            {props.isAnswer && <AnswerExplanation {...props}></AnswerExplanation>}
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.question}>
+        {props.questionNumber}. {props.question.question}
+      </h2>
+      {props.isBookmark ? (
+        <FontAwesomeIcon
+          icon={solidBookmark}
+          onClick={props.onBookmark.bind(null, props.questionNumber)}
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faBookmark}
+          onClick={props.onBookmark.bind(null, props.questionNumber)}
+        />
+      )}
+      <div className={styles.choices}>
+        {props.question.choices &&
+          props.question.choices.map((choice, index) => (
+            <Choice
+              key={index}
+              data={choice}
+              index={index}
+              onChoose={answerQuestionHandler}
+            />
+          ))}
+      </div>
+      {!props.isAnswer && (
+        <div className={styles.navigation}>
+          {!props.isFirst && (
+            <Button onClick={props.onPrevQuestion}>ข้อก่อนหน้า</Button>
+          )}
+          {!props.isLast && (
+            <Button onClick={props.onNextQuestion}>ข้อถัดไป</Button>
+          )}
+          {props.isLast && (
+            <Button onClick={submitAnswerHandler}>ส่งคำตอบ</Button>
+          )}
         </div>
-
-    )
+      )}
+      {props.isAnswer && <AnswerExplanation {...props}></AnswerExplanation>}
+    </div>
+  );
 }
