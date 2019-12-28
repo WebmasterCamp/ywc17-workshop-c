44d4c8c1a35af66356e78390abf6f93976291e52
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 11:14:12 +0700

    c

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 import styles from './DashboardPage.module.css';
 
-import Button from '../../shared/UI/Button/Button';
-import Question from '../../shared/UI/Question/Question';
-import questions from '../../shared/data/questions';
-
+import Card from '../../shared/UI/Card/Card';
 
 export default function DashboardPage() {
     return (
