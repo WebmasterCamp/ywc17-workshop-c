0dc6907334be461169140395d81bdadd271dcc69
Pittawat Taveekitworachai <pittawat2542@gmail.com>
2019-12-28 09:43:50 +0700

    c

--- src/pages/SummaryPage/SummaryPage.js
+++ src/pages/SummaryPage/SummaryPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import styles from './SummaryPage.module.css';
+import Chart from '../../shared/UI/Chart/Chart';
 
 export default function SummaryPage() {
     return (
         <div>
-            SummaryPage
+            <h1>SummaryPage</h1>
+             <Chart />
         </div>
     )
 }
